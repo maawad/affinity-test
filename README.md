dc6b990933c64eef5b29c2cc24f409213e07f7b3
Muhammad Awad <mawad@ucdavis.edu>
2021-11-10 21:10:06 -0800

    Add tested code

--- README.md
+++ README.md
@@ -1,1 +1,104 @@
-# affinity-test
+# Test CUDA MPS for a single user
+
+
+See [Multi-Process Service](https://docs.nvidia.com/deploy/pdf/CUDA_Multi_Process_Service_Overview.pdf) for more details.
+
+# How to test:
+
+1. First start the  MPS control daemon by exporting the following:
+```bash
+export CUDA_VISIBLE_DEVICES=0
+export CUDA_MPS_PIPE_DIRECTORY=/home/username/mps/mps
+export CUDA_MPS_LOG_DIRECTORY=/home/username/mps/log
+# or any location that are accessible to the user
+```
+
+2. After starting the daemon `nvidia-smi` should show something like this:
+
+```bash
+$ nvidia-smi
+Wed Nov 10 20:50:01 2021
++-----------------------------------------------------------------------------+
+| NVIDIA-SMI 495.29.05    Driver Version: 495.29.05    CUDA Version: 11.5     |
+|-------------------------------+----------------------+----------------------+
+| GPU  Name        Persistence-M| Bus-Id        Disp.A | Volatile Uncorr. ECC |
+| Fan  Temp  Perf  Pwr:Usage/Cap|         Memory-Usage | GPU-Util  Compute M. |
+|                               |                      |               MIG M. |
+|===============================+======================+======================|
+|   0  Quadro GV100        On   | 00000000:18:00.0 Off |                  Off |
+| 29%   29C    P2    36W / 250W |    151MiB / 32508MiB |      0%      Default |
+|                               |                      |                  N/A |
++-------------------------------+----------------------+----------------------+
+
++-----------------------------------------------------------------------------+
+| Processes:                                                                  |
+|  GPU   GI   CI        PID   Type   Process name                  GPU Memory |
+|        ID   ID                                                   Usage      |
+|=============================================================================|
+|    0   N/A  N/A    664535      C   nvidia-cuda-mps-server             27MiB |
++-----------------------------------------------------------------------------+
+```
+
+3. Build and run:
+```bash
+mkdir build
+cd build
+make
+./affinity_test
+```
+
+The output should look like this:
+```bash
+Device[0]: Quadro GV100
+SMs count: 80
+Producer SMs = 1/80
+Consumer SMs = 79/80
+Creating producer_ctx
+Creating consumer_ctx
+Launching kernels
+Ok.
+```
+
+If the GPU doesn't support MPS, you should see something like this:
+```bash
+$ ./affinity_test
+Device[0]: NVIDIA GeForce RTX 2070 with Max-Q Design
+SMs count: 36
+CUDA error at /home/username/GitHub/affinity-test/affinity_test.cu 58: cudaErrorUnsupportedExecAffinity
+terminate called without an active exception
+Aborted
+```
+
+4. Checking the run on the profiler:
+
+```bash
+nsys profile -o nsys_profile ./affinity_test
+```
+
+5. Import the output file in `nsys-ui`
+
+![](/figs/nsys-output.PNG)
+
+The test code contains a simple Message passing (MP) example. Where one kernel is a producer and the other is a consumer.
+
+
+The test also contains a simple test (copy kernel). Here is the SM utilization for the simple test on `nvprof`:
+
+Using one SM (notice that the requested one SM was rounded up to two):
+![](/figs/2sms-load.PNG)
+
+
+Using 50% of the SMs (notice that the SMs are assigned randomly):
+![](/figs/50psms-load.PNG)
+
+To test `nvprof` profile the kernel as follows:
+```bash
+nvprof --analysis-metrics -f -o affinity_test.nvvp  ./affinity_test
+```
+Then run the `nvvp` on the host machine. Here is how to launch it on Windows:
+```
+nvvp -vm "C:\Program Files\Java\jre1.8.0_291\bin\java.exe"
+```
+
+
+
--- affinity_test.cu
+++ affinity_test.cu
@@ -3,10 +3,16 @@
 #include <cuda.h>
 #include <cuda_runtime_api.h>
 
+// CUDA
+#include <thrust/device_vector.h>
+#include <thrust/host_vector.h>
+
 // C++
+#include <assert.h>
 #include <cstdint>
 #include <iostream>
 #include <numeric>
+#include <thread>
 #include <vector>
 
 #define cuda_try(call)                                                                \
@@ -43,19 +49,184 @@ CUdevice get_cuda_device(const int device_id, int& sms_count) {
   return device;
 }
 
-int main(int argc, char** argv) {
+__global__ void kernel(uint32_t* d_in, uint32_t* d_out, int count) {
+  auto thread_id = blockIdx.x * blockDim.x + threadIdx.x;
+  for (int i = thread_id; i < count; i += blockDim.x * gridDim.x) {
+    d_out[i] = d_in[i];
+  }
+}
+
+void one_kernel_test(int argc, char** argv) {
   int device_id = 0;
   int sms_count = 0;
   CUdevice dev = get_cuda_device(device_id, sms_count);
 
   float load = 0.5f;
-  int load_sms = static_cast<int>(load * sms_count);
+  unsigned load_sms = static_cast<unsigned>(load * sms_count);
+  load_sms = 1;
   CUcontext ctx;
   CUexecAffinityParam_v1 affinity_param{
       CUexecAffinityType::CU_EXEC_AFFINITY_TYPE_SM_COUNT, load_sms};
   auto flags = CUctx_flags::CU_CTX_SCHED_AUTO;
 
+  std::cout << "Requesting " << load_sms << " SMs\n";
   cuda_try(cuCtxCreate_v3(&ctx, &affinity_param, 1, flags, dev));
 
+  CUexecAffinityParam acquired_affinity_param;
+
+  cuda_try(
+      cuCtxGetExecAffinity(&acquired_affinity_param, CU_EXEC_AFFINITY_TYPE_SM_COUNT));
+  std::cout << "Acquired " << acquired_affinity_param.param.smCount.val << " SMs\n";
+
+  uint32_t count = 100'000'000;
+  uint32_t* d_in;
+  uint32_t* d_out;
+  std::size_t bytes_count = sizeof(uint32_t) * count;
+  cuda_try(cudaMalloc(&d_in, bytes_count));
+  cuda_try(cudaMalloc(&d_out, bytes_count));
+  cuda_try(cudaMemset(d_in, 0xff, bytes_count));
+  cuda_try(cudaMemset(d_out, 0x00, bytes_count));
+
+  const uint32_t block_size = 128;
+  const uint32_t num_blocks = 512;
+
+  kernel<<<num_blocks, block_size>>>(d_in, d_out, count);
+  cuda_try(cudaDeviceSynchronize());
+
+  std::vector<uint32_t> h_out(count);
+
+  cuda_try(cudaMemcpy(h_out.data(), d_out, bytes_count, cudaMemcpyDeviceToHost));
+
+  for (const auto& k : h_out) {
+    assert(k == 0xffffffff);
+  }
+
+  cuda_try(cudaFree(d_in));
+  cuda_try(cudaFree(d_out));
   cuda_try(cuCtxDestroy(ctx));
+
+  std::cout << "Ok\n";
+}
+
+template <typename T>
+__device__ inline T load_acquire(T* ptr) {
+  T old;
+  asm volatile("ld.acquire.gpu.b32 %0,[%1];" : "=r"(old) : "l"(ptr) : "memory");
+  return old;
+}
+
+template <typename T>
+__device__ inline void store_release(T* ptr, T value) {
+  asm volatile("st.release.gpu.b32 [%0], %1;" ::"l"(ptr), "r"(value) : "memory");
+}
+
+__managed__ bool success_;
+
+__global__ void producer_kernel(uint32_t* data,
+                                uint32_t* flags,
+                                uint32_t count,
+                                uint32_t produce) {
+  auto stride = blockDim.x * gridDim.x;
+  auto thread_index = threadIdx.x + blockIdx.x * blockDim.x;
+  for (auto tid = thread_index; tid < count; tid += stride) {
+    data[tid] = produce;
+    uint32_t* line_flag = flags + tid / 32;
+    store_release(line_flag, 1u);
+  }
+}
+__global__ void consumer_kernel(uint32_t* data,
+                                uint32_t* flags,
+                                uint32_t count,
+                                uint32_t consume) {
+  auto stride = blockDim.x * gridDim.x;
+  auto thread_index = threadIdx.x + blockIdx.x * blockDim.x;
+  for (auto tid = thread_index; tid < count; tid += stride) {
+    uint32_t* line_flag = flags + tid / 32;
+    while (!load_acquire(line_flag))
+      ;  // spin till flag is set
+    auto found = data[tid];
+    if (consume != found) {
+      success_ = false;
+    }
+  }
+}
+
+void test_producer_consumer(int argc, char** argv) {
+  uint32_t count = (argc >= 2) ? std::atoi(argv[1]) : 100'000'000;
+  float producer_sms_ratio = (argc >= 3) ? std::atof(argv[2]) : 0.0125f;
+  float consumer_sms_ratio = (argc >= 4) ? std::atoi(argv[3]) : 1.0f - producer_sms_ratio;
+
+  int device_id = 0;
+  int sms_count = 0;
+  CUdevice device = get_cuda_device(device_id, sms_count);
+
+  unsigned producer_sms = static_cast<unsigned>(producer_sms_ratio * sms_count);
+  unsigned consumer_sms = static_cast<unsigned>(consumer_sms_ratio * sms_count);
+  producer_sms = std::max(producer_sms, 1u);
+  consumer_sms = std::max(consumer_sms, 1u);
+
+  std::cout << "Producer SMs = " << producer_sms << "/" << sms_count << '\n';
+  std::cout << "Consumer SMs = " << consumer_sms << "/" << sms_count << '\n';
+
+  // Setup context and affinity parameters
+  CUexecAffinityParam_v1 producer_affinity_param{
+      CUexecAffinityType::CU_EXEC_AFFINITY_TYPE_SM_COUNT, producer_sms};
+  CUexecAffinityParam_v1 consumer_affinity_param{
+      CUexecAffinityType::CU_EXEC_AFFINITY_TYPE_SM_COUNT, consumer_sms};
+
+  auto affinity_flags = CUctx_flags::CU_CTX_SCHED_AUTO;
+
+  CUcontext producer_ctx, consumer_ctx;
+  std::cout << "Creating producer_ctx\n";
+  cuda_try(
+      cuCtxCreate_v3(&producer_ctx, &producer_affinity_param, 1, affinity_flags, device));
+  std::cout << "Creating consumer_ctx\n";
+  cuda_try(
+      cuCtxCreate_v3(&consumer_ctx, &consumer_affinity_param, 1, affinity_flags, device));
+
+  // Allocate memory
+  uint32_t* data;
+  uint32_t* flags;
+  std::size_t bytes_count = sizeof(uint32_t) * count;
+  cuda_try(cudaMalloc(&data, bytes_count));
+  cuda_try(cudaMalloc(&flags, bytes_count));
+  cuda_try(cudaMemset(data, 0x00, bytes_count));
+  cuda_try(cudaMemset(flags, 0x00, bytes_count));
+  success_ = true;
+  cuda_try(cudaDeviceSynchronize());
+  std::cout << "Launching kernels\n";
+  // launch kernels
+  const uint32_t block_size = 128;
+  const uint32_t num_blocks = 512;
+
+  // producer kernel
+  std::thread producer_thread{[&] {
+    cuCtxSetCurrent(producer_ctx);
+    producer_kernel<<<num_blocks, block_size>>>(data, flags, count, 512);
+  }};
+
+  // consumer kernel
+  std::thread consumer_thread{[&] {
+    cuCtxSetCurrent(consumer_ctx);
+    consumer_kernel<<<num_blocks, block_size>>>(data, flags, count, 512);
+  }};
+  producer_thread.join();
+  consumer_thread.join();
+
+  cuda_try(cudaDeviceSynchronize());
+  if (success_) {
+    std::cout << "Ok.\n";
+  } else {
+    std::cout << "Error\n";
+  }
+  cuda_try(cudaFree(data));
+  cuda_try(cudaFree(flags));
+
+  cuda_try(cuCtxDestroy(producer_ctx));
+  cuda_try(cuCtxDestroy(consumer_ctx));
+}
+
+int main(int argc, char** argv) {
+  // one_kernel_test(argc, argv);
+  test_producer_consumer(argc, argv);
 }
