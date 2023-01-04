64bf19c800d9dbce68fbadae6fe98af062c71b5c
maawad <mawad@ucdavis.edu>
2023-01-04 03:14:40 -0800

    Add HIP code

--- affinity_test.cu
+++ affinity_test.cu
@@ -1,9 +1,8 @@
 ﻿
-// CUDA driver & runtime
-#include <cuda.h>
-#include <cuda_runtime_api.h>
+// HiP driver & runtime
+#include "hip/hip_runtime.h"
 
-// CUDA
+// HiP
 #include <thrust/device_vector.h>
 #include <thrust/host_vector.h>
 
@@ -15,31 +14,31 @@
 #include <thread>
 #include <vector>
 
-#define cuda_try(call)                                                                \
-  do {                                                                                \
-    cudaError_t err = static_cast<cudaError_t>(call);                                 \
-    if (err != cudaSuccess) {                                                         \
-      printf("CUDA error at %s %d: %s\n", __FILE__, __LINE__, cudaGetErrorName(err)); \
-      std::terminate();                                                               \
-    }                                                                                 \
+#define hip_try(call)                                                               \
+  do {                                                                              \
+    hipError_t err = static_cast<hipError_t>(call);                                 \
+    if (err != hipSuccess) {                                                        \
+      printf("HIP error at %s %d: %s\n", __FILE__, __LINE__, hipGetErrorName(err)); \
+      std::terminate();                                                             \
+    }                                                                               \
   } while (0)
 
-CUdevice get_cuda_device(const int device_id, int& sms_count) {
-  CUdevice device;
+hipDevice_t get_hip_device(const int device_id, int& sms_count) {
+  hipDevice_t device;
   int device_count = 0;
 
-  cuda_try(cuInit(0));  // Flag parameter must be zero
-  cuda_try(cuDeviceGetCount(&device_count));
+  hip_try(hipInit(0));  // Flag parameter must be zero
+  hip_try(hipGetDeviceCount(&device_count));
 
   if (device_count == 0) {
-    std::cout << "No CUDA capable device found." << std::endl;
+    std::cout << "No HIP capable device found." << std::endl;
     std::terminate();
   }
 
-  cuda_try(cuDeviceGet(&device, device_id));
+  hip_try(hipDeviceGet(&device, device_id));
 
-  cudaDeviceProp device_prop;
-  cudaGetDeviceProperties(&device_prop, device_id);
+  hipDeviceProp_t device_prop;
+  hipGetDeviceProperties(&device_prop, device_id);
 
   sms_count = device_prop.multiProcessorCount;
 
@@ -59,65 +58,66 @@ __global__ void kernel(uint32_t* d_in, uint32_t* d_out, int count) {
 void one_kernel_test(int argc, char** argv) {
   int device_id = 0;
   int sms_count = 0;
-  CUdevice dev = get_cuda_device(device_id, sms_count);
+  hipDevice_t dev = get_hip_device(device_id, sms_count);
 
   float load = 0.5f;
   unsigned load_sms = static_cast<unsigned>(load * sms_count);
 
-  CUcontext ctx;
-  CUexecAffinityParam_v1 affinity_param{
-      CUexecAffinityType::CU_EXEC_AFFINITY_TYPE_SM_COUNT, load_sms};
-  auto flags = CUctx_flags::CU_CTX_SCHED_AUTO;
+  hipCtx_t ctx;
+  auto flags = 0;  // FIXME
 
   std::cout << "Requesting " << load_sms << " SMs (load = " << load << ")\n";
-  cuda_try(cuCtxCreate_v3(&ctx, &affinity_param, 1, flags, dev));
-
-  CUexecAffinityParam acquired_affinity_param;
-  cuda_try(
-      cuCtxGetExecAffinity(&acquired_affinity_param, CU_EXEC_AFFINITY_TYPE_SM_COUNT));
-  unsigned acquired_sms = acquired_affinity_param.param.smCount.val;
-  std::cout << "Acquired " << acquired_sms << " SMs\n";
+  // FIXME
+  /// git/affinity_test.cu:70:11: warning: 'hipCtxCreate' is deprecated: This API is
+  /// marked as deprecated and may not be supported in future releases. For more details
+  /// please refer
+  /// https://github.com/ROCm-Developer-Tools/HIP/blob/master/docs/markdown/hip_deprecated_api_list.md
+  /// [-Wdeprecated-declarations]
+  hip_try(hipCtxCreate(&ctx, flags, dev));
 
   uint32_t count = 100'000'000;
   uint32_t* d_in;
   uint32_t* d_out;
   std::size_t bytes_count = sizeof(uint32_t) * count;
-  cuda_try(cudaMalloc(&d_in, bytes_count));
-  cuda_try(cudaMalloc(&d_out, bytes_count));
-  cuda_try(cudaMemset(d_in, 0xff, bytes_count));
-  cuda_try(cudaMemset(d_out, 0x00, bytes_count));
+  hip_try(hipMalloc(&d_in, bytes_count));
+  hip_try(hipMalloc(&d_out, bytes_count));
+  hip_try(hipMemset(d_in, 0xff, bytes_count));
+  hip_try(hipMemset(d_out, 0x00, bytes_count));
 
   const uint32_t block_size = 128;
   const uint32_t num_blocks = 512;
 
   kernel<<<num_blocks, block_size>>>(d_in, d_out, count);
-  cuda_try(cudaDeviceSynchronize());
+  hip_try(hipDeviceSynchronize());
 
   std::vector<uint32_t> h_out(count);
 
-  cuda_try(cudaMemcpy(h_out.data(), d_out, bytes_count, cudaMemcpyDeviceToHost));
+  hip_try(hipMemcpy(h_out.data(), d_out, bytes_count, hipMemcpyDeviceToHost));
 
   for (const auto& k : h_out) {
     assert(k == 0xffffffff);
   }
 
-  cuda_try(cudaFree(d_in));
-  cuda_try(cudaFree(d_out));
-  cuda_try(cuCtxDestroy(ctx));
+  hip_try(hipFree(d_in));
+  hip_try(hipFree(d_out));
+  hip_try(hipCtxDestroy(ctx));
 
   std::cout << "Ok\n";
 }
 
 template <typename T>
 __device__ inline T load_acquire(T* ptr) {
-  T old;
-  asm volatile("ld.acquire.gpu.b32 %0,[%1];" : "=r"(old) : "l"(ptr) : "memory");
+  T old = *reinterpret_cast<volatile T*>(ptr);
+  __threadfence();
+  // asm volatile("ld.acquire.gpu.b32 %0,[%1];" : "=r"(old) : "l"(ptr) : "memory");
   return old;
 }
 
 template <typename T>
 __device__ inline void store_release(T* ptr, T value) {
-  asm volatile("st.release.gpu.b32 [%0], %1;" ::"l"(ptr), "r"(value) : "memory");
+  __threadfence();
+  *reinterpret_cast<volatile T*>(ptr) = value;
+  // asm volatile("st.release.gpu.b32 [%0], %1;" ::"l"(ptr), "r"(value) : "memory");
 }
 
 __managed__ bool success_;
@@ -158,7 +158,7 @@ void test_producer_consumer(int argc, char** argv) {
 
   int device_id = 0;
   int sms_count = 0;
-  CUdevice device = get_cuda_device(device_id, sms_count);
+  hipDevice_t device = get_hip_device(device_id, sms_count);
 
   unsigned producer_sms = static_cast<unsigned>(producer_sms_ratio * sms_count);
   unsigned consumer_sms = static_cast<unsigned>(consumer_sms_ratio * sms_count);
@@ -169,31 +169,29 @@ void test_producer_consumer(int argc, char** argv) {
   std::cout << "Consumer SMs = " << consumer_sms << "/" << sms_count << '\n';
 
   // Setup context and affinity parameters
-  CUexecAffinityParam_v1 producer_affinity_param{
-      CUexecAffinityType::CU_EXEC_AFFINITY_TYPE_SM_COUNT, producer_sms};
-  CUexecAffinityParam_v1 consumer_affinity_param{
-      CUexecAffinityType::CU_EXEC_AFFINITY_TYPE_SM_COUNT, consumer_sms};
+  // CUexecAffinityParam_v1 producer_affinity_param{
+  //     CUexecAffinityType::CU_EXEC_AFFINITY_TYPE_SM_COUNT, producer_sms};
+  // CUexecAffinityParam_v1 consumer_affinity_param{
+  //     CUexecAffinityType::CU_EXEC_AFFINITY_TYPE_SM_COUNT, consumer_sms};
 
-  auto affinity_flags = CUctx_flags::CU_CTX_SCHED_AUTO;
+  auto affinity_flags = 0;
 
-  CUcontext producer_ctx, consumer_ctx;
+  hipCtx_t producer_ctx, consumer_ctx;
   std::cout << "Creating producer_ctx\n";
-  cuda_try(
-      cuCtxCreate_v3(&producer_ctx, &producer_affinity_param, 1, affinity_flags, device));
+  hip_try(hipCtxCreate(&producer_ctx, affinity_flags, device));
   std::cout << "Creating consumer_ctx\n";
-  cuda_try(
-      cuCtxCreate_v3(&consumer_ctx, &consumer_affinity_param, 1, affinity_flags, device));
+  hip_try(hipCtxCreate(&consumer_ctx, affinity_flags, device));
 
   // Allocate memory
   uint32_t* data;
   uint32_t* flags;
   std::size_t bytes_count = sizeof(uint32_t) * count;
-  cuda_try(cudaMalloc(&data, bytes_count));
-  cuda_try(cudaMalloc(&flags, bytes_count));
-  cuda_try(cudaMemset(data, 0x00, bytes_count));
-  cuda_try(cudaMemset(flags, 0x00, bytes_count));
+  hip_try(hipMalloc(&data, bytes_count));
+  hip_try(hipMalloc(&flags, bytes_count));
+  hip_try(hipMemset(data, 0x00, bytes_count));
+  hip_try(hipMemset(flags, 0x00, bytes_count));
   success_ = true;
-  cuda_try(cudaDeviceSynchronize());
+  hip_try(hipDeviceSynchronize());
   std::cout << "Launching kernels\n";
   // launch kernels
   const uint32_t block_size = 128;
@@ -201,29 +199,29 @@ void test_producer_consumer(int argc, char** argv) {
 
   // producer kernel
   std::thread producer_thread{[&] {
-    cuCtxSetCurrent(producer_ctx);
+    hipCtxSetCurrent(producer_ctx);
     producer_kernel<<<num_blocks, block_size>>>(data, flags, count, 512);
   }};
 
   // consumer kernel
   std::thread consumer_thread{[&] {
-    cuCtxSetCurrent(consumer_ctx);
+    hipCtxSetCurrent(consumer_ctx);
     consumer_kernel<<<num_blocks, block_size>>>(data, flags, count, 512);
   }};
   producer_thread.join();
   consumer_thread.join();
 
-  cuda_try(cudaDeviceSynchronize());
+  hip_try(hipDeviceSynchronize());
   if (success_) {
     std::cout << "Ok.\n";
   } else {
     std::cout << "Error\n";
   }
-  cuda_try(cudaFree(data));
-  cuda_try(cudaFree(flags));
+  hip_try(hipFree(data));
+  hip_try(hipFree(flags));
 
-  cuda_try(cuCtxDestroy(producer_ctx));
-  cuda_try(cuCtxDestroy(consumer_ctx));
+  hip_try(hipCtxDestroy(producer_ctx));
+  hip_try(hipCtxDestroy(consumer_ctx));
 }
 
 int main(int argc, char** argv) {
